3c9f981223016c464d3210485b665de3b4777ea3
SolidifiedRay <yc3346@nyu.edu>
2022-11-10 17:11:17 -0500

    Add functions that show the histogram

--- otsu.py
+++ otsu.py
@@ -1,17 +1,44 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-def grayConversion(image):
-    grayValue = 0.114 * image[:,:,2] + 0.587 * image[:,:,1] + 0.299 * image[:,:,0]
+
+def rgbToGray(image):
+    # 𝐼 = 𝑅ound(0.299𝑅 + 0.587𝐺 + 0.114𝐵)
+    grayValue = 0.299 * image[:,:,0] + 0.587 * image[:,:,1] + 0.114 * image[:,:,2]
     gray_img = grayValue.astype(np.uint8)
     return gray_img
 
-def histogram(grayScale):
-    return
 
-orig = cv2.imread(r'basket_balls.bmp', 1)
-g = grayConversion(orig)
-cv2.imshow("Original", orig)
-cv2.imshow("GrayScale", g)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def getHistogramArray(grayscaleImage):
+    histgoramArray = [0] * 256
+    for row in grayscaleImage:
+        for pixel in row:
+            histgoramArray[pixel] += 1
+    return histgoramArray
+
+
+def showHistogram(grayscaleImage):
+    flatList = [item for sublist in grayscaleImage for item in sublist]
+    bins = [0] * 256
+    for i in range(1, 256):
+        bins[i] = bins[i-1] + 1
+    plt.hist(flatList, bins=bins)
+    plt.show()
+
+
+def showImage(image, name = "image"):
+    # press 0 key to close images
+    cv2.imshow(name, image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def main():
+    originalImage = cv2.imread(r'basket_balls.bmp', 1)
+    grayscaleImage = rgbToGray(originalImage)
+    showImage(grayscaleImage)
+    showHistogram(grayscaleImage)
+
+
+main()
